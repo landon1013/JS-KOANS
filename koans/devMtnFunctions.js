2df05727c9a19a4ce6cd8f19cfcbfafbfe380292
Landon1013 <landon@thecallfamily.com>
2014-09-17 14:25:40 -0600

    updates all pages

--- koans/devMtnFunctions.js
+++ koans/devMtnFunctions.js
@@ -13,14 +13,18 @@ describe('First Functions', function(){
     //to a variable called currentYearVar
     var getYear = function() {
         var current = new Date().getFullYear();
-        var currentYearVar = getYear();
+        return current;
     }
+
+    var currentYearVar = getYear();
 
     expect(currentYearVar).toBe(2014);
   });
   it('should create a multiply function', function(){
     //Write a function called multiply that takes in two parametrs, multiplies them together, and returns the answer
-
+    var multiply = function (num1, num2) {
+        return num1 * num2
+    }
     expect(multiply(2,4)).toBe(8);
   });
 })
